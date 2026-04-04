4f18f8b89e2cac62dd0c471dd814fe23bc9e77f3
ViditRajpurohit <viditrajpurohit27@gmail.com>
2026-04-04 10:04:50 +0530

    Created login/sign up page

--- Frontend/src/App.tsx
+++ Frontend/src/App.tsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import AppLayout from './AppLayout/AppLayout';
-import { Home } from 'lucide-react';
+import Home from './pages/Home'
 import Tools from './pages/Tools';
 import Community from './pages/Community';
 import Resources from './components/Resources';
+import SignUpPage from './pages/SignUpPage';
+import LoginPage from './pages/LoginPage';
 
 const App=()=> {
 
@@ -15,8 +17,11 @@ const App=()=> {
       <Route path="/home" element={<Home/>}/>
       <Route path="/tools" element={<Tools/>}/>
       <Route path="/community" element={<Community/>}/>
-      <Route path="/resources" element={<Resources/>}/>
+          <Route path="/resources" element={<Resources />} />
       </Route>
+        <Route path='/signup' element={<SignUpPage />} />
+        <Route path='/login' element={<LoginPage />} />
+        
       </Routes> 
     </BrowserRouter>
   )
